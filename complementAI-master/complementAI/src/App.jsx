71f6d3a4c5b64fc87c420509265dcd2d3b078c68
JeanSR <jea.sanchezr@duocuc.cl>
2025-09-03 19:19:37 -0400

    Agrego APIia

---
+++ complementAI-master/complementAI/src/App.jsx
@@ -0,0 +1,12 @@
+import CreateProject from './CreateProject';
+import './App.css'; // Opcional, puedes limpiar los estilos por defecto
+
+function App() {
+  return (
+    <div className="App">
+      <CreateProject />
+    </div>
+  );
+}
+
+export default App;
